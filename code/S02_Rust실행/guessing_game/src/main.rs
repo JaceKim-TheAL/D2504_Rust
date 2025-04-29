e40f48faf8875add18b737b057523dea359fca64
Jace Kim <jacekim.theal@gmail.com>
2025-04-30 08:46:45 +0900

    S02. Rust 실행

--- code/S02_Rust실행/guessing_game/src/main.rs
+++ code/S02_Rust실행/guessing_game/src/main.rs
@@ -1,27 +1,14 @@
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
-
 
     println!("Please input your guess.");
+
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
+    io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("너무 작아요!"),
-        Ordering::Greater => println!("너무 커요!"),
-        Ordering::Equal => println!("You win !!!")
-    }
+    println!("You guessed: {}", guess);
 }
