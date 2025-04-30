28fd78d73d9c0a34db36d3184949ce0ab098f047
Jace Kim <jacekim.theal@gmail.com>
2025-04-30 09:27:42 +0900

    S02. Rust 실행

--- code/S02_Rust실행/guessing_game/target/debug/build/getrandom-f03d0de3f39cdd5b/build_script_build-f03d0de3f39cdd5b.d
+++
@@ -1,5 +0,0 @@
-C:\GitHub\D2504_Rust\code\S02_Rust실행\guessing_game\target\debug\build\getrandom-f03d0de3f39cdd5b\build_script_build-f03d0de3f39cdd5b.exe: C:\Users\bigpy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.2\build.rs
-
-C:\GitHub\D2504_Rust\code\S02_Rust실행\guessing_game\target\debug\build\getrandom-f03d0de3f39cdd5b\build_script_build-f03d0de3f39cdd5b.d: C:\Users\bigpy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.2\build.rs
-
-C:\Users\bigpy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.3.2\build.rs:
--- code/S02_Rust실행/guessing_game/target/debug/deps/cfg_if-19d394c6436e68ea.d
+++
@@ -1,7 +0,0 @@
-C:\GitHub\D2504_Rust\code\S02_Rust실행\guessing_game\target\debug\deps\libcfg_if-19d394c6436e68ea.rmeta: C:\Users\bigpy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\GitHub\D2504_Rust\code\S02_Rust실행\guessing_game\target\debug\deps\libcfg_if-19d394c6436e68ea.rlib: C:\Users\bigpy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\GitHub\D2504_Rust\code\S02_Rust실행\guessing_game\target\debug\deps\cfg_if-19d394c6436e68ea.d: C:\Users\bigpy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\bigpy\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs:
