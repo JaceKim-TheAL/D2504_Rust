771a69c5caa5efe48e36071d0179b963be25f7b3
Jace Kim <jacekim.theal@gmail.com>
2025-04-30 13:58:12 +0900

    Clone Rust 2nd-edition

---
+++ book/second-edition/build-and-push-to-gh-pages.sh
@@ -0,0 +1,16 @@
+cd ..
+rev=$(git rev-parse --short HEAD)
+cd second-edition
+rm -rf book
+mdbook build
+cd book
+touch .nojekyll
+git init
+git remote add upstream "https://rinthel@github.com/rinthel/rust-lang-book-ko"
+git fetch upstream
+git reset upstream/gh-pages
+touch .
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
+cd ..
